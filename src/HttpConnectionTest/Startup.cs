b1de903bc939d7b8f8b6ea4f0e56df5533bac2bb
Paul Knopf <Paul Knopf>
2016-01-07 01:01:50 -0800

    Adding project that produces the issue

---
+++ src/HttpConnectionTest/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Http.Features;
+
+namespace HttpConnectionTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                var httpConnection = context.Features.Get<IHttpConnectionFeature>();
+                if(httpConnection == null)
+                {
+                    await context.Response.WriteAsync("No connection feature...");
+                }
+                else
+                {
+                    await context.Response.WriteAsync("IP is " + httpConnection.RemoteIpAddress);
+                }
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
